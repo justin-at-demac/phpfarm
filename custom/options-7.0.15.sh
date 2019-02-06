f8b2512a56a6d268aae0f2566030aac06cdaa2ef
Justin <justin-at-demac@users.noreply.github.com>
2019-02-06 10:00:46 -0500

    Create options-7.0.15.sh

---
+++ custom/options-7.0.15.sh
@@ -0,0 +1,34 @@
+# options-7.0.15.sh
+# Like options-7.sh
+
+configoptions="
+--enable-bcmath \
+--enable-calendar \
+--enable-cli \
+--enable-exif \
+--enable-fpm \
+--enable-ftp \
+--enable-gd-native-ttf \
+--enable-intl \
+--enable-mbstring \
+--enable-pcntl \
+--enable-soap \
+--enable-sockets \
+--enable-wddx \
+--enable-zip \
+--with-curl \
+--with-freetype-dir=/usr/include/freetype2 \
+--with-gd \
+--with-gettext \
+--with-jpeg-dir \
+--with-mcrypt \
+--with-mysql \
+--with-mysqli \
+--with-openssl \
+--with-pdo-mysql \
+--with-pear \
+--with-png-dir \
+--with-readline \
+--with-xsl \
+--with-zlib
+"
